86a09690b428d757c4b13eac50545ba278c8c43e
Nikita Ekert <ekertn7@gmail.com>
2024-07-08 17:12:39 +0300

    Create environment for gigachat help function.

---
+++ SberMegaParser/tools/gigahelp/gigahelp.py
@@ -0,0 +1,16 @@
+__all__ = ['gigahelp']
+
+
+TOKEN_GIGACHAT = ''
+
+
+def gigahelp(url: str, query: str) -> str:
+    """Основная функция для выдачи результата по запросу пользователя."""
+
+
+def _crop_html(html: str) -> str:
+    """Приватная функция для обрезки html."""
+
+
+def _ask_gigachat(query: str) -> str:
+    """Запрос к API гигачата."""
