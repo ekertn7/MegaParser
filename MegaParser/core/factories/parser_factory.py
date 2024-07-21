fbe5ff6b4979551b6323a66e5cdc216f06e130db
Nikita Ekert <ekertn7@gmail.com>
2024-07-21 23:56:27 +0300

    Fix imports and variable names

--- MegaParser/core/factories/parser_factory.py
+++ MegaParser/core/factories/parser_factory.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 
-__all__ = ['ParserFactory',
-           'create_nulls_collection']
+__all__ = ['ParserFactory', 'create_nulls_collection']
 
 
 def create_nulls_collection(length: int):
@@ -9,7 +8,6 @@ def create_nulls_collection(length: int):
 
 
 class ParserFactory(ABC):
-
     @abstractmethod
     def get_parsers(self):
-        pass
+        """Abstract method to get parsers."""
